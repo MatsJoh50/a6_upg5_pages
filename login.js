7a3cca55c822844f6c857fd9c510089c2f56b812
Mats Johansson <matjoh50@gmail.com>
2024-03-06 14:53:03 +0100

    working profile page with update and logout function

--- login.js
+++ login.js
@@ -7,16 +7,15 @@ const submitInfo = document.querySelector("#submitFormInfo");
 // Check if the user is logged in and redirect to the user profile page if they are
 const loggedInUser = window.localStorage.getItem("loginUser");
 if (loggedInUser) {
-    window.location.href = "./userprofile.html";
+  window.location.href = "./userprofile.html";
 }
 
 // Show the login form after a short delay
 const showLoginForm = () => {
-    loginForm.classList.remove("d-none");
+  loginForm.classList.remove("d-none");
 };
 
 setTimeout(showLoginForm, 50);
-
 
 loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -26,32 +25,29 @@ loginForm.addEventListener("submit", (event) => {
   const fetchUser = username.replace(/\./g, "") + domain.split(".")[0];
   const existingUser = JSON.parse(window.localStorage.getItem(fetchUser));
 
-
   try {
     if (existingUser != null) {
       console.log("user exists");
       if (existingUser.password === loginPassword.value) {
         console.log("Pass works");
         console.log("Welcome Master");
-        
-        existingUser.username = fetchUser;
-        window.localStorage.setItem("loginUser", JSON.stringify(existingUser))
-        window.location.href = "./userprofile.html";
 
-      }  else {
+        existingUser.username = fetchUser;
+        window.localStorage.setItem("loginUser", JSON.stringify(existingUser));
+        window.location.href = "./userprofile.html";
+      } else {
         console.log("uh uh uuuh, nope");
-        submitInfo.innerText = "Wrong username or password"
-        email.classList.add("border-danger")
-        loginPassword.classList.add("border-danger")
+        submitInfo.innerText = "Wrong username or password";
+        email.classList.add("border-danger");
+        loginPassword.classList.add("border-danger");
         setTimeout(() => {
-          submitInfo.innerText ="";
-          email.classList.remove("border-danger")
-          loginPassword.classList.remove("border-danger")
-        }, 5000)
-      } 
+          submitInfo.innerText = "";
+          email.classList.remove("border-danger");
+          loginPassword.classList.remove("border-danger");
+        }, 5000);
+      }
     }
   } catch (e) {
     console.log(e);
   }
-
 });
--- userprofile.js
+++ userprofile.js
@@ -1,88 +1,100 @@
 // Retrieve the loginUser string from localStorage
 const loginUserString = window.localStorage.getItem("loginUser");
-try{
 
-    const getProfileName = JSON.parse(loginUserString)
-    const activeProfile = window.localStorage.getItem(getProfileName.username);
-    console.log(activeProfile)
+  const getProfileName = JSON.parse(loginUserString);
+  const getActiveProfile = window.localStorage.getItem(getProfileName.username);
+  const activeProfile = JSON.parse(getActiveProfile);
+  console.log("Login profile:",activeProfile);
 
-} catch(e){console.log(e)};
+if (activeProfile) {
+  // Parse the cleaned loginUser string as JSON
+  try {
+    const firstName = document.querySelector("#firstName");
+    if (activeProfile.firstName !== undefined) {
+      firstName.value = activeProfile.firstName;
+    }
+  } catch {}
 
-if (loginUserString) {
-    // Parse the cleaned loginUser string as JSON
-    const loginUser = JSON.parse(loginUserString);
-    try {
-        if(loginUser.firstName !== undefined){
-            const firstName = document.querySelector("#firstName");
-            firstName.value = loginUser.firstName;
-
-        }
-    } catch {}
-
-    try {
-        if(loginUser.lastName !== undefined){
-            const lastName = document.querySelector("#lastName");
-            lastName.value = loginUser.lastName;
-        }
-    } catch {}
-    try {
-        if(loginUser.email !== undefined){
-            const email = document.querySelector("#email");
-            email.value = loginUser.email;
-            
-        }
-    } catch {}
-    try {
-        if(loginUser.phonenumber !== undefined){
-            const phonenumber = document.querySelector("#phonenumber");
-            phonenumber.value = loginUser.phonenumber;
-            
-        }
-    } catch {}
-    try {
-        if(loginUser.homeAdress !== undefined){
-            const homeAdress = document.querySelector("#homeAdress");
-            homeAdress.value = loginUser.homeAdress;
-        }
-    } catch {}
-    try {
-        if(loginUser.city !== undefined){
-            const city = document.querySelector("#city");
-            city.value = loginUser.city
-        }
-    } catch {}
-    try {
-        if(loginUser.zipcode !== undefined){
-            const zipcode = document.querySelector("#zipcode");
-            zipcode.value = loginUser.zipcode
-            
-        }
-    } catch {}
+  try {
+    const lastName = document.querySelector("#lastName");
+    if (activeProfile.lastName !== undefined) {
+      lastName.value = activeProfile.lastName;
+    }
+  } catch {}
+  try {
+    const email = document.querySelector("#email");
+    if (activeProfile.email !== undefined) {
+      email.value = activeProfile.email;
+    }
+  } catch {}
+  try {
+    const phonenumber = document.querySelector("#phonenumber");
+    if (activeProfile.phonenumber !== undefined) {
+      phonenumber.value = activeProfile.phonenumber;
+    }
+  } catch {}
+  try {
+    const homeAdress = document.querySelector("#homeAdress");
+    if (activeProfile.homeAdress !== undefined) {
+      homeAdress.value = activeProfile.homeAdress;
+    }
+  } catch {}
+  try {
+    const city = document.querySelector("#city");
+    if (activeProfile.city !== undefined) {
+      city.value = activeProfile.city;
+    }
+  } catch {}
+  try {
+    const zipcode = document.querySelector("#zipcode");
+    if (activeProfile.zipcode !== undefined) {
+      zipcode.value = activeProfile.zipcode;
+    }
+  } catch {}
 } else {
   console.error("No loginUser data found in localStorage");
 }
 
-function setValue(query) {
-    try {
-        // Check if loginUser object exists and has the property specified by 'query'
-        if (loginUser && loginUser[query] !== undefined) {
-            const element = document.querySelector(`#${query}`);
-            if (element) {
-                element.value = loginUser[query];
-            } else {
-                console.error(`Element with ID '${query}' not found`);
-            }
-        }
-    } catch (error) {
-        console.error('Error setting value:', error);
-    }
-}
 
-
-const logout = document.querySelector("#logout");
 
 const profile = document.querySelector("#profileInfo");
 profile.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(loginUser);
+
+  if(firstName.value !== activeProfile.firstName){
+    activeProfile.firstName = firstName.value;
+  }
+
+  if(lastName.value !== activeProfile.lastName){
+    activeProfile.lastName = lastName.value;
+  }
+  
+  if(email.value !== activeProfile.email){
+    activeProfile.email = email.value;
+  }
+  
+  if(phonenumber.value !== activeProfile.phonenumber){
+    activeProfile.phonenumber = phonenumber.value;
+  }
+  
+  if(homeAdress.value !== activeProfile.homeAdress){
+    activeProfile.homeAdress = homeAdress.value;
+  }
+  
+  if(city.value !== activeProfile.city){
+    activeProfile.city = city.value;
+  }
+  
+  if(zipcode.value !== activeProfile.zipcode){
+    activeProfile.zipcode = zipcode.value;
+  }
+window.localStorage.setItem(activeProfile.username, JSON.stringify(activeProfile))
+
+  console.log(activeProfile);
+});
+
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", () =>{
+  window.localStorage.removeItem("loginUser")
+  window.location.href ="./login.html"
 });
